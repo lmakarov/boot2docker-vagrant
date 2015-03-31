baadd5740397db2ff49fd19c3d9974e2984c3f0d
Leonid Makarov <leonid.makarov@blinkreaction.com>
2015-03-31 17:25:23 -0400

    Update setup.sh
    More commentary and colors during the installation.
    Better handling of the automatic DOCKER_HOST variable export based on the shell used.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,66 @@
+#!/bin/bash
+
+# Console colors
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[1;33m'
+NC='\033[0m'
+
 # Homebrew installation
+echo -e "${green}Installing Homebrew...${NC}"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Cask installation
+echo -e "${green}Installing Cask...${NC}"
 brew install caskroom/cask/brew-cask
 
-# VirtualBox and Vagrant installation
-brew cask install virtualbox vagrant
+# VirtualBox installation
+echo -e "${green}Installing virtualbox...${NC}"
+brew cask install virtualbox
 
-# Install docker and docker-compose
-brew install docker docker-compose
+# Vagrant installation
+echo -e "${green}Installing vagrant...${NC}"
+brew cask install vagrant
 
-# Download Vagrantfile and start the boot2docker VM
-curl -O https://raw.githubusercontent.com/blinkreaction/boot2docker-vagrant/master/Vagrantfile && vagrant up
+# Install docker
+echo -e "${green}Installing docker...${NC}"
+brew install docker
 
-# Export DOCKER_HOST variable in ~/.bash_profile
-echo -e '\n# Docker (default for Vagrant based boxes)\nexport DOCKER_HOST=tcp://localhost:2375\n' >> ~/.bash_profile
-source ~/.bash_profile
+# Install docker-compose
+echo -e "${green}Installing docker-compose...${NC}"
+brew install docker-compose
+
+# Download Vagrantfile
+echo -e "${green}Downloading Vagrantfile into the current directory...${NC}"
+curl -O https://raw.githubusercontent.com/blinkreaction/boot2docker-vagrant/master/Vagrantfile
+
+# Start the boot2docker VM
+echo -e "${green}Starting the boot2docker VM...${NC}"
+vagrant up
+
+# Write DOCKER_HOST variable export to a matching .rc file based on the shell (bash or zsh)
+SOURCE_FILE='';
+DOCKER_HOST_EXPORT='\n# Docker (default for Vagrant based boxes)\nexport DOCKER_HOST=tcp://localhost:2375\n'
+
+# Detect shell to write to the right .rc file
+if [[ $SHELL == '/bin/bash' ]]; then SOURCE_FILE=".bashrc"; fi
+if [[ $SHELL == '/bin/zsh' ]]; then	SOURCE_FILE=".zshrc"; fi
+
+if [[ $SOURCE_FILE ]]; then
+	# See if we already did this and skip if so
+	grep -Rq "export DOCKER_HOST=tcp://localhost:2375" $HOME/$SOURCE_FILE
+	if [[ $? -ne 0 ]]; then
+		echo -e "${green}Adding automatic DOCKER_HOST export to $HOME/$SOURCE_FILE${NC}"
+		echo -e $DOCKER_HOST_EXPORT >> $HOME/$SOURCE_FILE
+	fi
+else
+	echo -e "${red}Cannot detect your shell. Please manually add the following to your respective .rc or .profile file:${NC}"
+	echo -e "$DOCKER_HOST_EXPORT"
+fi
 
 # Check that Docker works
+echo -e "${green}Checking that everything is in place...${NC}"
 docker version
-docker-compose --version
+if [[ $? -ne 0 ]]; then
+	echo -e "${red}Something went wrong. Please review console output for possible clues."
+fi
