a90c7f0c79e67635526fdcb58ed1e5b573a5071e
Alexei Chekulaev <alexei.chekulaev@blinkreaction.com>
2015-07-17 15:48:55 -0400

    Create separate presetup cmd for Git Bash

---
+++ presetup-git-bash.cmd
@@ -0,0 +1,11 @@
+REM Installation chocolatey
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+REM Install virtualbox, vagrant, docker
+choco install virtualbox -y
+choco install vagrant -y
+choco install docker -y
+
+REM Git configuration (proper line endings on Windows and support for long paths)
+git config --global core.autocrlf input
+git config --system core.longpaths true
