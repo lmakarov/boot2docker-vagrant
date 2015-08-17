f333188294684be355ca14686a064eede8b97999
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-08-17 22:11:30 +0300

    presetup-win.sh

---
+++ presetup-win.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install prerequisites via choco (virtualbox and vagrant)
+curl -L https://raw.githubusercontent.com/blinkreaction/boot2docker-vagrant/feature/windows/presetup-win.cmd -o $WINDIR/Temp/presetup-win.cmd
+curl -L -O https://raw.githubusercontent.com/blinkreaction/boot2docker-vagrant/feature/windows/presetup-win.vbs -o $WINDIR/Temp/presetup-win.vbs
+cscript $WINDIR/Temp/presetup-win.vbs
+
+# Install Docker
+curl -L https://get.docker.com/builds/Windows/i386/docker-1.8.1.exe -o /usr/local/bin/docker
+chmod +x /usr/local/bin/docker
+
+# Install Docker Compose (via pip)
+pact install python-setuptools libxml2-devel libxslt-devel libyaml-devel
+curl -skS https://bootstrap.pypa.io/get-pip.py | python
+pip install virtualenv
+curl -skS https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
+pip install -U docker-compose==1.4.0
