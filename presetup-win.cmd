3fa3d63675e87b6e33c08722953a34bd939d1236
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-08-18 13:55:09 +0300

    README updates

--- presetup-win.cmd
+++ presetup-win.cmd
@@ -1,6 +1,6 @@
-REM Install Chocolatey
+REM Installing Chocolatey
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-REM Install virtualbox, vagrant
+REM Installing virtualbox, vagrant
 choco install virtualbox -y
 choco install vagrant -y
