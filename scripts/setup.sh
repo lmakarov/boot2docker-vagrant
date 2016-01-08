6274a811a4a47435b53967efaaa5e7d72a7745be
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-01-08 17:28:51 -0500

    Set point DOCKER_HOST to 192.168.10.10 instead of localhost
    localhost works via the NAT VirtualBox interface, which shows poor performance with Intel based virtual adapters and docker.
    Virtio adapter type is not affected, but may have it's own issues.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,9 @@ green='\033[0;32m'
 yellow='\033[1;33m'
 NC='\033[0m'
 
+# Docker Host IP
+DOCKER_HOST_IP='192.168.10.10'
+
 # For testing
 if [ ! $B2D_BRANCH == "" ]; then
 	echo -e "${red}[b2d-setup] testing mode: environment = \"${B2D_BRANCH}\"$NC"
@@ -23,7 +26,7 @@ curl -sO "https://raw.githubusercontent.com/blinkreaction/boot2docker-vagrant/${
 
 # Write DOCKER_HOST variable export to a matching .rc file based on the shell (bash or zsh)
 SOURCE_FILE=''
-DOCKER_HOST_EXPORT='\n# Docker (default for Vagrant based boxes)\nexport DOCKER_HOST=tcp://localhost:2375\n'
+DOCKER_HOST_EXPORT='\n# Docker (default for Vagrant based boxes)\nexport DOCKER_HOST=tcp://${DOCKER_HOST_IP}:2375\n'
 
 # Detect shell to write to the right .rc file
 if [[ $SHELL == '/bin/bash' || $SHELL == '/bin/sh' ]]; then SOURCE_FILE=".bash_profile"; fi
@@ -31,7 +34,7 @@ if [[ $SHELL == '/bin/zsh' ]]; then	SOURCE_FILE=".zshrc"; fi
 
 if [[ $SOURCE_FILE ]]; then
 	# See if we already did this and skip if so
-	grep -q "export DOCKER_HOST=tcp://localhost:2375" $HOME/$SOURCE_FILE
+	grep -q "export DOCKER_HOST=tcp://${DOCKER_HOST_IP}:2375" $HOME/$SOURCE_FILE
 	if [[ $? -ne 0 ]]; then
 		echo -e "${green}Adding automatic DOCKER_HOST export to $HOME/$SOURCE_FILE${NC}"
 		echo -e $DOCKER_HOST_EXPORT >> $HOME/$SOURCE_FILE
