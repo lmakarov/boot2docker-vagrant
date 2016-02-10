6de60378c4cfe4c39fac407bce7247fb7da480cd
Wilfried JEANNIARD <willou.com@gmail.com>
2016-02-10 16:59:49 +0100

    Adding version support for docker and docker-compose

--- scripts/presetup-win.cmd
+++ scripts/presetup-win.cmd
@@ -8,12 +8,17 @@ if %errorLevel% == 0 (
     exit
 )
 
+SET version_docker=1.9.1
+SET version_dockercompose=1.5.2
+
 REM *** Installing Chocolatey ***
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 REM *** Installing Packages ***
-choco install virtualbox vagrant git docker docker-compose -y
-choco upgrade virtualbox vagrant git docker docker-compose -y
+choco install virtualbox vagrant git -y
+choco upgrade virtualbox vagrant git -y
+choco install docker -y -version %version_docker%
+choco install docker-compose -y -version %version_dockercompose%
 
 REM *** Refresh PATH ***
 SET PATH=%PATH%
